a8753c51a0405d39895decb30cf3763bb423162a
Karthik Ramesh <rk.karthikramesh@gmail.com>
2015-01-28 18:12:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,43 @@ var main=function()
         scrollTop: $(".project4").offset().top
     }, 2700,'easeOutExpo');
     });
+    
+    
+    $(function(){
+ 
+	$(document).on( 'scroll', function(){
+ 
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+});
+
+$(function(){
+ 
+	$(document).on( 'scroll', function(){
+ 
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 1000, 'swing');
+}
+
+//http://www.webtipblog.com/adding-scroll-top-button-website
 }   
     /*$('.dropdown-toggle').click(function(){
      $('.dropdown-menu').toggle();
